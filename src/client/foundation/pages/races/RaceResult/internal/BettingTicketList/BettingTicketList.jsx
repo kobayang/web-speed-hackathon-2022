4d42760fe1fb4f6c9df61fd0a6a3bda3dcaaa116
kobayang <naokiod@gmail.com>
2022-11-19 10:59:04 +0900

    Remove fontawesome assets and render icon directly

--- src/client/foundation/pages/races/RaceResult/internal/BettingTicketList/BettingTicketList.jsx
+++ src/client/foundation/pages/races/RaceResult/internal/BettingTicketList/BettingTicketList.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import { EntryCombination } from "../../../../../components/displays/EntryCombination";
+import { TicketAlt } from "../../../../../components/icons/Icon";
 import { Color, FontSize, Space } from "../../../../../styles/variables";
 
 const ItemWrapper = styled.tr`
@@ -60,7 +61,8 @@ export const BettingTicketList = ({ children }) => {
   if (React.Children.count(children) === 0) {
     return (
       <Placeholder>
-        <i className="fas fa-ticket-alt" />
+        {/* <i className="fas fa-ticket-alt" /> */}
+        <TicketAlt />
         <div>購入した拳券はありません</div>
       </Placeholder>
     );
